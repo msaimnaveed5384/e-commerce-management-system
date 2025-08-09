c023cd043bb8cc859eca4b0b1eeec4cd2daadfa7
Muhammad Saim Naveed <msaimnaveed5384@gmail.com>
2025-08-09 22:48:22 +0500

    Create AuthSystem.h

---
+++ AuthSystem.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "FileManager.h"
+#include "User.h"
+#include <iostream>
+#include <string>
+#include <conio.h>
+//Login/registration logic with validation
+//File-based persistence for storing registered users
+User* Login();
+void Registration();
